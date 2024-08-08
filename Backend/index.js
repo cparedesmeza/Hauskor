ed7595c923267d49e542b45a1f78074af999b680
cparedes <cparedesmeza@gmail.com>
2024-08-07 19:36:35 -0600

    Modificación de backend port

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,14 @@
 'use strict'
+require('dotenv').config();
 
 var mongoose = require('mongoose'); // para utilizar el modulo que se instalo desde node.js
 var app = require('./app');
-const port = process.env.Port || 3900;
+const port = process.env.port;
 //Crear servidor y ponerme a escuchar peticiones http
 app.listen(port, () => {
     console.log('Servidor corriendo en http://localhost:' + port);
 });
-
-const url = 'mongodb+srv://pruebaslocales51:12345@dbmongotest.eejnz.mongodb.net/'
+const url = process.env.MONGODB_URI;
 
 //mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
