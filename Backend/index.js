3b8b7e4648066bbca60439fffa13ec454081b06d
cparedes <cparedesmeza@gmail.com>
2024-08-07 18:58:34 -0600

    Modificación de backend port

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,16 @@
 var mongoose = require('mongoose'); // para utilizar el modulo que se instalo desde node.js
 var app = require('./app');
 const port = process.env.Port || 3900;
-const url =  'mongodb+srv://pruebaslocales51:12345@dbmongotest.eejnz.mongodb.net/'
+//Crear servidor y ponerme a escuchar peticiones http
+app.listen(port, () => {
+    console.log('Servidor corriendo en http://localhost:' + port);
+});
+
+const url = 'mongodb+srv://pruebaslocales51:12345@dbmongotest.eejnz.mongodb.net/'
 
 //mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect(url)
-    .then(()=>{
-            console.log('Conexion a base de datos correcta!')
-
-            //Crear servidor y ponerme a escuchar peticiones http
-            app.listen(port, () =>{
-                console.log('Servidor corriendo en http://localhost:' + port);
-            });
+    .then(() => {
+        console.log('Conexion a base de datos correcta!')
     })
